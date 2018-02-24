3da2a3b841e827ccdd1869b1e94acfdaf418b8b9
sergaidm <sergaidm@yandex.ru>
2018-02-24 10:04:10 +0200

    Program4

--- Program4/src/ru/yandex/sergaidm/Main.java
+++ Program4/src/ru/yandex/sergaidm/Main.java
@@ -21,33 +21,33 @@ public class Main {
 		Student nine = new Student("Sergeeva", 19, "female", "KU");
 		Student ten = new Student("Konstantinova", 17, "female", "TU");
 
-		 Group gr = new Group();
-		
-		 try {
-		 gr.addStudentToGroup(one, 0);
-		 gr.addStudentToGroup(two, 1);
-		 gr.addStudentToGroup(three, 2);
-		 gr.addStudentToGroup(four, 3);
-		 gr.addStudentToGroup(five, 4);
-		 gr.addStudentToGroup(six, 5);
-		 gr.addStudentToGroup(seven, 6);
-		 gr.addStudentToGroup(eight, 7);
-		 gr.addStudentToGroup(nine, 8);
-		 gr.addStudentToGroup(ten, 9);
-		 } catch (GroupException e) {
-		 System.out.println(e);
-		 }
-		 System.out.println("\n" + gr + "\n");
+		Group gr = new Group();
 
-		 Group gr2 = new Group();
-		 for (int i = 0; i < 3; i++) {
-		 try {
-		 gr2.interactiveAddStudentToGroup();
-		 } catch (IllegalArgumentException e) {
-		 System.out.println(e);
-		 }
-		 }
-		 System.out.println("\n" + gr2 + "\n");
+		try {
+			gr.addStudentToGroup(one, 0);
+			gr.addStudentToGroup(two, 1);
+			gr.addStudentToGroup(three, 2);
+			gr.addStudentToGroup(four, 3);
+			gr.addStudentToGroup(five, 4);
+			gr.addStudentToGroup(six, 5);
+			gr.addStudentToGroup(seven, 6);
+			gr.addStudentToGroup(eight, 7);
+			gr.addStudentToGroup(nine, 8);
+			gr.addStudentToGroup(ten, 9);
+		} catch (GroupException e) {
+			System.out.println(e);
+		}
+		System.out.println("\n" + gr + "\n");
+
+		Group gr2 = new Group();
+		for (int i = 0; i < 3; i++) {
+			try {
+				gr2.interactiveAddStudentToGroup();
+			} catch (IllegalArgumentException e) {
+				System.out.println(e);
+			}
+		}
+		System.out.println("\n" + gr2 + "\n");
 
 		Student[] students = { one, two, three, four, five, six, seven, eight, nine, null };
 		System.out.println("Unsorted array of students: " + "\n");
@@ -105,12 +105,12 @@ public class Main {
 			System.out.println(student);
 		}
 
-		 MilitaryCommissar mc = gr;
-		 Student[] arrayStudent = gr.isRecruit();
-		 System.out.println("\n" + "Students - recruits: " + "\n");
-		 for (Student student2 : arrayStudent) {
-		 System.out.println(student2);
-		 
-		 }
+		MilitaryCommissar mc = gr;
+		Student[] arrayStudent = gr.isRecruit();
+		System.out.println("\n" + "Students - recruits: " + "\n");
+		for (Student student2 : arrayStudent) {
+			System.out.println(student2);
+
+		}
 	}
 }
