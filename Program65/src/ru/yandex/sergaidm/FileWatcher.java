f1aef1986e0825aba702979e99d69e2a9e8d684a
sergaidm <sergaidm@yandex.ru>
2018-03-15 14:45:23 +0200

    Program65

--- Program65/src/ru/yandex/sergaidm/FileWatcher.java
+++ Program65/src/ru/yandex/sergaidm/FileWatcher.java
@@ -9,10 +9,12 @@ import java.util.concurrent.TimeUnit;
 public class FileWatcher implements Runnable {
 
 	private File mainFolder;
+	private File[] filesArray;
 
 	public FileWatcher(File mainFolder) {
 		super();
 		this.mainFolder = mainFolder;
+		filesArray = this.mainFolder.listFiles();
 	}
 
 	public FileWatcher() {
@@ -23,29 +25,25 @@ public class FileWatcher implements Runnable {
 		return mainFolder;
 	}
 
-	private static String getFolderInfo(File mainFolder) {
+	private String getFolderInfo() {
 		if (mainFolder == null) {
 			return "None";
 		}
 		File[] fileArray = mainFolder.listFiles();
-
-		int l = fileArray.length;
 		String text = "";
-		System.out.println(fileArray.length);
-
-		if (l > fileArray.length) {
+		if (filesArray.length < fileArray.length) {
 			text = "New file is created" + System.lineSeparator();
 		}
-		if (l < fileArray.length) {
+		if (filesArray.length > fileArray.length) {
 			text = "File is deleted" + System.lineSeparator();
 		}
+		filesArray = fileArray;
 		return text;
 	}
 
-	public static void startFileWatcher() {
-		File mainFolder = new File(".");
+	public void startFileWatcher() {
 		ScheduledExecutorService service = Executors.newSingleThreadScheduledExecutor();
-		service.scheduleAtFixedRate(new FileWatcher(mainFolder), 0, 1, TimeUnit.SECONDS);
+		service.scheduleAtFixedRate(this, 0, 1, TimeUnit.SECONDS);
 		try {
 			service.awaitTermination(60, TimeUnit.SECONDS);
 		} catch (InterruptedException e) {
@@ -60,11 +58,9 @@ public class FileWatcher implements Runnable {
 
 	@Override
 	public void run() {
-
 		SimpleDateFormat sdf = new SimpleDateFormat("dd:MM:yyyy   hh:mm:ss");
 		System.out.println(sdf.format(System.currentTimeMillis()));
-		System.out.println(getFolderInfo(mainFolder));
-
+		System.out.println(getFolderInfo());
 	}
 
 }
--- Program65/src/ru/yandex/sergaidm/Main.java
+++ Program65/src/ru/yandex/sergaidm/Main.java
@@ -1,10 +1,13 @@
 package ru.yandex.sergaidm;
 
+import java.io.File;
+
 public class Main {
 
 	public static void main(String[] args) {
-				
-		FileWatcher.startFileWatcher();
 		
+		File file = new File(".");
+		FileWatcher fw = new FileWatcher(file);
+		fw.startFileWatcher();
 	}
 }
