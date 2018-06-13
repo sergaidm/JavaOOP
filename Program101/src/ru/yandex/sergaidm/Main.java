0788b68224593516dc5ecd96dd5368c5fd063f1d
sergaidm <sergaidm@yandex.ru>
2018-06-13 16:16:43 +0300

    10. Stream API. Find students by the first letter of the surname

--- Program101/src/ru/yandex/sergaidm/Main.java
+++ Program101/src/ru/yandex/sergaidm/Main.java
@@ -1,9 +1,10 @@
 package ru.yandex.sergaidm;
 
 public class Main {
+	
 	public static void main(String[] args) {
+		
 		Group group = new Group();
-
 		group.addStudent("Ivanov", 22.5, 'm', "DP-51s");
 		group.addStudent("Petrov", 23, 'm', "DP-51s");
 		group.addStudent("Sidorov", 22.8, 'm', "DP-51s");
@@ -12,8 +13,8 @@ public class Main {
 		group.addStudent("Kaktusov", 23.1, 'm', "DP-51s");
 		group.addStudent("Andreeva", 23, 'f', "DP-51s");
 		group.addStudent("Sergeeva", 22.9, 'f', "DP-51s");
-		group.printGroup();	
-		
+		group.printGroup();			
 		System.out.println(group.searchStudentByLetter('S'));
 	}
+	
 }
