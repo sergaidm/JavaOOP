13efc96b110f7566207c160284aadc325fd0bb68
sergaidm <sergaidm@yandex.ru>
2018-06-13 16:20:54 +0300

    10. Stream API. Array of numbers by ASSII-codes of letters

--- Program103/src/ru/yandex/sergaidm/Main.java
+++ Program103/src/ru/yandex/sergaidm/Main.java
@@ -5,8 +5,10 @@ import java.util.Arrays;
 public class Main {
 
 	public static void main(String[] args) {
+		
 		String string = "This string will be presented in ASSII-format";
 		System.out.println(Arrays.toString(stringToASSIIArray(string)));
+		
 	}
 
 	public static int[] stringToASSIIArray(String text) {
