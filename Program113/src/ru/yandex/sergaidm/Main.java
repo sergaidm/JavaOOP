de9a8ad604ced2f71fab417ff9d11b9159dfeb82
sergaidm <sergaidm@yandex.ru>
2018-06-13 16:56:52 +0300

    11. Map. Console text translator to ASSII-art

--- Program113/src/ru/yandex/sergaidm/Main.java
+++ Program113/src/ru/yandex/sergaidm/Main.java
@@ -1,17 +1,25 @@
 package ru.yandex.sergaidm;
 
+import java.io.File;
 import java.util.Scanner;
 
 public class Main {
 
 	public static void main(String[] args) {
 
-		ASSIIArtLetters as = new ASSIIArtLetters();
+		ConsoleTranslator consoleTranslator = new ConsoleTranslator();
+		File font = new File("Big letters.txt");
+		consoleTranslator.loadFontFromFile(font);
+		font = new File("Small letters.txt");
+		consoleTranslator.loadFontFromFile(font);
+		font = new File("Punctuation marks.txt");
+		consoleTranslator.loadFontFromFile(font);
 		Scanner sc = new Scanner(System.in);
-		System.out.println("Input text: ");
-		String text = as.transform(sc.nextLine());
-		System.out.println(text);
-		
+		System.out.println("Input text to convert to ASSII-art: ");
+		String text = sc.nextLine();
+		sc.close();
+		System.out.println(consoleTranslator.transform(text));
+
 	}
 
 }
